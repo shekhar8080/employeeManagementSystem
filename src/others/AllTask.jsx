6a111ce9cc4f39f079fa82c13ba9cf2b5f65107d
shekhar <shekharrvds@gmail.com>
2025-11-03 22:57:17 +0530

    All task list comoleted

---
+++ src/others/AllTask.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div id='allTask' className='bg-[#1c1c1c] p-5 mt-3 h-40 overflow-auto rounded'>
+        <div className='flex justify-between mb-3 bg-amber-400 px-4 py-2 rounded'>
+            <h2>Shekhar</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='flex justify-between mb-3 bg-blue-400 px-4 py-2 rounded'>
+            <h2>Shekhar</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='flex justify-between mb-3 bg-green-400 px-4 py-2 rounded'>
+            <h2>Shekhar</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='flex justify-between mb-3 bg-purple-400 px-4 py-2 rounded'>
+            <h2>Shekhar</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='flex justify-between mb-3 bg-red-400 px-4 py-2 rounded'>
+            <h2>Shekhar</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+    </div>
+  )
+}
+
+export default AllTask
