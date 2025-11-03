c924f8c98cb2ee19a9d3c4916f310f5616e63fac
shekhar <shekharrvds@gmail.com>
2025-11-03 22:56:31 +0530

    Admin dashboard is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './authPage/Login'
 import EmpDashboard from './dashboard/EmpDashboard'
+import AdminDashboard from './dashboard/AdminDashboard'
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
-      <EmpDashboard />
+      {/* <EmpDashboard /> */}
+      <AdminDashboard />
     </>
   )
 }
--- src/dashboard/AdminDashboard.jsx
+++ src/dashboard/AdminDashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../others/Header'
+import CreateTask from '../others/CreateTask'
+import AllTask from '../others/AllTask'
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <div className='p-10 h-screen w-full'>
+      <Header />
+      <CreateTask />
+      <AllTask />
+    </div>
   )
 }
 
